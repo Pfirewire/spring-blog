104128b4684ea149ec8419f8c9dacc1cb7e901f1
Stephen Pflug <stephen@pflug.us>
2022-11-15 14:10:34 -0600

    moved files from other repo

---
+++ src/main/java/com/codeup/springblog/SpringBootExercisesApplication.java
@@ -0,0 +1,14 @@
+package com.codeup.springblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Application class
+@SpringBootApplication
+public class SpringBootExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExercisesApplication.class, args);
+    }
+
+}
